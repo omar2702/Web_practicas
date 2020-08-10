dbdb305139104de73bf1d5ce83576aea4dd67758
Omar <34077377+omar2702@users.noreply.github.com>
2020-08-10 16:47:56 -0500

    vale Carlos aqui esta

---
+++ Supermarket.API/Controllers/CategoryController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Supermarket.API.Domain.Models;
+using Supermarket.API.Domain.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Supermarket.API.Controllers
+{
+    [Route("/api[controller]")]
+    public class CategoryController : Controller
+    {
+        private ICategoryService _categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        public async Task<IEnumerable<Category>> GetAllAsync()
+        {
+            var categories = await _categoryService.ListAsync();
+            return categories;
+        }
+    }
+}
---
+++ Supermarket.API/Domain/Models/Products.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Supermarket.API.Domain.Models
+{
+    public class Products
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public short QuantityInPackage { get; set; }
+
+        public EUnit0fMeasurement EUnit0fMeasurement { get; set; }
+
+        //Poblara list de productos en una columna en la clase Products
+        public int categoryId { get; set;  }
+        public Category Category { get; set; }
+    }
+}
